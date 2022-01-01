37aceb19d70dafd7f88d2d7a79606cc419f9ed9a
unknown <66206428+IloveScientis@users.noreply.github.com>
2022-01-01 18:46:28 +0700

    sign in issue

--- force-app/main/default/lwc/lwc3dforms/lwc3dforms.js
+++ force-app/main/default/lwc/lwc3dforms/lwc3dforms.js
@@ -4,15 +4,16 @@ export default class Lwc3dforms extends LightningElement {
 
     signin(){
         const formsWrapper = this.template.querySelector('.forms-wrapper');
-               this.template.querySelector('.signup-btn').addEventListener('click',(event) =>{
+               this.template.querySelector('.signin-btn').addEventListener('click',(event) =>{
          event.preventDefault();
          formsWrapper.classList.add("change");
     });
     }
     signup(){
+        
         const formsWrapper = this.template.querySelector('.forms-wrapper');
      
-        this.template.querySelector('.signin-btn').addEventListener('click',(event) =>{
+        this.template.querySelector('.signup-btn').addEventListener('click',(event) =>{
              event.preventDefault();
              formsWrapper.classList.add("change");
         });
